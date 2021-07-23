50c1adaac605faa7afb25fbdb62eb8d9c6bb41a4
bkfrye <brandonkfrye@gmail.com>
2021-07-23 13:08:05 -0400

    add main nav to masthead

--- components/Masthead.js
+++ components/Masthead.js
@@ -4,8 +4,16 @@ import styles from '../styles/Masthead.module.css';
 const Masthead = () => {
 
   return (
-    <div className={styles.container}>
-      <Logo className={styles.logo} />
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <Logo className={styles.logo} />
+        <nav className={styles.nav}>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Shop</a>
+          <a href="#">About us</a>
+        </nav>
+      </div>
     </div>
   )
 }
