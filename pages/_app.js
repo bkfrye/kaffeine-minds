0da9d74f729937298ac065f7ffe58a9f46e5604c
bkfrye <brandonkfrye@gmail.com>
2021-07-29 12:15:09 -0400

    include favicon and meta data

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,17 @@ import Footer from '@components/Footer.js';
 import Head from 'next/head';
 import Masthead from '@components/Masthead.js';
 
+import metaImg from '@images/kaffeine-minds.jpg';
+
 function App({ Component, pageProps }) {
   return (
     <AppWrapper>
       <Head>
         <title>Kaffeine Minds</title>
-        <meta name="description" content="Kaffeine Minds website" />
+        <meta name="description" content="Kaffeine Minds is coffee for developers" />
+        <meta property="og:title" content="Kaffeine Minds - coffee for developers" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={metaImg} />
       </Head>
       <Masthead />
       <main>
